65632a67dfe6791903a06670ef6ac283e0968bad
Jamile <95001489+meunovosite@users.noreply.github.com>
2024-09-04 17:58:28 -0300

    Update index.tsx

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -6,9 +6,7 @@ export default function Link() {
     return (
         <Style>
             <NextLink href={URL} passHref>
-            <a href={URL} target="_blank" rel="noopener noreferrer">
-                    Conheça o dev
-                </a>
+                <a href={URL} target="_blank" rel="noopener noreferrer">Conheça o dev</a>
             </NextLink>
         </Style>
     );
